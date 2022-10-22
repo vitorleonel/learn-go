501b44f79b50cde8c492b971d86d271551c87f9f
Vitor Leonel <falecom@vitorleonel.com>
2022-10-22 18:49:51 +0100

    feat: starting project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	router := gin.Default();
+
+	router.GET("/", func (context *gin.Context)  {
+		context.JSON(200, gin.H{"message": "it's working!"})
+	})
+
+	router.Run(":3000");
+}
